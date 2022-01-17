60106251c4fa2320a5766d032169a421318208bd
Himalaya <yugmani@yahoo.com>
2022-01-17 00:26:53 -0800

    javascript

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const newYears = "1 Jun 2022";
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secsEl = document.getElementById("secs");
 
 function countDown() {
   const newYearDate = new Date(newYears);
   const currentDate = new Date();
 
-  const seconds = (newYearDate - currentDate) / 1000;
+  const total_seconds = (newYearDate - currentDate) / 1000;
 
-  const days = Math.floor(seconds / 3600 / 24);
-  //   const hours;
-  console.log(days);
+  const days = Math.floor(total_seconds / 3600 / 24);
+  const hours = Math.floor((total_seconds / 3600) % 24);
+  const minutes = Math.floor((total_seconds / 60) % 60);
+  const seconds = Math.floor(total_seconds % 60);
+
+  daysEl.innerHTML = formatTime(days);
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(minutes);
+  secsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
 }
 
 countDown();
 
-// setInterval(countDown, 1000);
+setInterval(countDown, 1000);
